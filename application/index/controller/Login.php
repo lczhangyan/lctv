a086063d507856de27739405e33092c9b3003a1a
zhangyan <zhangyan@lcbtn.com>
2018-05-28 15:17:09 +0800

    添加密码修改方法

--- application/index/controller/Login.php
+++ application/index/controller/Login.php
@@ -25,6 +25,7 @@ class Login extends Controller
             if($password==$pass->password){
                 session("user_type",$pass->usertype);
                 session("name",$pass->name);
+                session("accounts",$name);
                 $data["msg"] = "成功";
                 $data["code"] = 0;
             }else{
@@ -44,5 +45,38 @@ class Login extends Controller
     public function logout(){
         session(null);
         return $this->fetch("login");
+    }
+    public function p_reset(){
+        return $this->fetch();
+    }
+    public function reset(){
+        header("Content-type: application/json; charset=UTF-8");
+        $request = $this->request;
+        $npass = $request->param("npass");
+        $npass1 = $request->param("npass1");
+        $resp = array();
+        if($npass!=$npass1){
+            $resp['code'] = 1;
+            $resp['msg'] = "密码不一致";
+        }else{
+            $accounts = session("accounts");
+            $M = new User();
+            $data = $M->where("accounts",$accounts)->find();
+            $password = $data->password;
+            $id = $data->id;
+            $pass = $request->param("pass");
+            $tem_password = md5($accounts."-".$pass);
+            if($tem_password!=$password){
+                $resp['code'] = 1;
+                $resp['msg'] = "原密码不正确";
+            }else{
+                $tem_password1 = md5($accounts.'-'.$npass);
+                $M->where('id',$id)->update(['password'=>$tem_password1]);
+                $resp['code'] = 0;
+                $resp['msg'] = "修改成功";
+            }
+        }
+        echo json_encode($resp,320);
+        exit();
     }
 }
